e5756ffbbfa8ddc45b6fe079c72f9e6a43c54bc5
cloudicen <cloudicen@foxmail.com>
2021-04-07 16:25:47 +0800

    修改了单词拼写错误

--- src/main/java/org/acowzon/backend/entity/goods/Comment.java
+++ src/main/java/org/acowzon/backend/entity/goods/Comment.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 
 @Data
 @NoArgsConstructor
-public class Comments implements Serializable {
+public class Comment implements Serializable {
     private String commentId; // 评论id
     private String goodsId; // 评论的商品id
     private String userId; // 用户id
@@ -17,7 +17,7 @@ public class Comments implements Serializable {
     private int commentScore; // 分数
     private Date commentTime; // 评论时间
 
-    public Comments(String goodsId, String userId, String commentText, int score) {
+    public Comment(String goodsId, String userId, String commentText, int score) {
         this.commentId = UUID.randomUUID().toString();
         this.goodsId = goodsId;
         this.userId = userId;
