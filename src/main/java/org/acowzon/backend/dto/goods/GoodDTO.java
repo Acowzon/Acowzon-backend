993098742360c094b78c08175e6686da952c4df7
cloudicen <cloudicen@foxmail.com>
2021-04-08 00:14:41 +0800

    添加缺失的字段

--- src/main/java/org/acowzon/backend/dto/goods/GoodDTO.java
+++ src/main/java/org/acowzon/backend/dto/goods/GoodDTO.java
@@ -4,10 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class GoodDTO {
+    private String goodsId;
     private String goodsName;
     private String goodsTypeId;
     private double goodsPrice;
@@ -19,109 +22,4 @@ public class GoodDTO {
     private int retailerId;
     private int goodsStarsCount;
     private int views;
-
-    @Override
-    public String toString() {
-        return "GoodDTO{" +
-                "goodsName='" + goodsName + '\'' +
-                ", goodsTypeId='" + goodsTypeId + '\'' +
-                ", goodsPrice=" + goodsPrice +
-                ", goodsImage='" + goodsImage + '\'' +
-                ", goodsSimpleDes='" + goodsSimpleDes + '\'' +
-                ", goodsDescription='" + goodsDescription + '\'' +
-                ", goodsInventory=" + goodsInventory +
-                ", soldCount=" + soldCount +
-                ", retailerId=" + retailerId +
-                ", goodsStarsCount=" + goodsStarsCount +
-                ", views=" + views +
-                '}';
-    }
-
-    public String getGoodsName() {
-        return goodsName;
-    }
-
-    public void setGoodsName(String goodsName) {
-        this.goodsName = goodsName;
-    }
-
-    public String getGoodsTypeId() {
-        return goodsTypeId;
-    }
-
-    public void setGoodsTypeId(String goodsTypeId) {
-        this.goodsTypeId = goodsTypeId;
-    }
-
-    public double getGoodsPrice() {
-        return goodsPrice;
-    }
-
-    public void setGoodsPrice(double goodsPrice) {
-        this.goodsPrice = goodsPrice;
-    }
-
-    public String getGoodsImage() {
-        return goodsImage;
-    }
-
-    public void setGoodsImage(String goodsImage) {
-        this.goodsImage = goodsImage;
-    }
-
-    public String getGoodsSimpleDes() {
-        return goodsSimpleDes;
-    }
-
-    public void setGoodsSimpleDes(String goodsSimpleDes) {
-        this.goodsSimpleDes = goodsSimpleDes;
-    }
-
-    public String getGoodsDescription() {
-        return goodsDescription;
-    }
-
-    public void setGoodsDescription(String goodsDescription) {
-        this.goodsDescription = goodsDescription;
-    }
-
-    public int getGoodsInventory() {
-        return goodsInventory;
-    }
-
-    public void setGoodsInventory(int goodsInventory) {
-        this.goodsInventory = goodsInventory;
-    }
-
-    public int getSoldCount() {
-        return soldCount;
-    }
-
-    public void setSoldCount(int soldCount) {
-        this.soldCount = soldCount;
-    }
-
-    public int getRetailerId() {
-        return retailerId;
-    }
-
-    public void setRetailerId(int retailerId) {
-        this.retailerId = retailerId;
-    }
-
-    public int getGoodsStarsCount() {
-        return goodsStarsCount;
-    }
-
-    public void setGoodsStarsCount(int goodsStarsCount) {
-        this.goodsStarsCount = goodsStarsCount;
-    }
-
-    public int getViews() {
-        return views;
-    }
-
-    public void setViews(int views) {
-        this.views = views;
-    }
 }
