4c282b5b31fc876e4d05ef898af2db01102d94b8
cloudicen <cloudicen@foxmail.com>
2021-04-16 00:05:33 +0800

    - 完成了用户管理服务接口的定义

--- src/main/java/org/acowzon/backend/service/user/UserService.java
+++ src/main/java/org/acowzon/backend/service/user/UserService.java
@@ -1,17 +1,95 @@
 package org.acowzon.backend.service.user;
 
 
-import org.acowzon.backend.dto.user.UserDTO;
-import org.acowzon.backend.entity.user.User;
+import org.acowzon.backend.dto.address.AddressDTO;
+import org.acowzon.backend.dto.user.UserBasicInfoDTO;
+import org.acowzon.backend.dto.user.UserFullInfoDTO;
+import org.acowzon.backend.exception.BusinessException;
 
+import java.util.UUID;
 
 public interface UserService {
 
-    User getUserInfo(String userID);
+    /**
+     * 获取用户的基本信息
+     * @param id 用户id
+     * @return UserBasicInfoDTO
+     * @throws BusinessException 业务相关异常
+     */
+    UserBasicInfoDTO getUserBasicInfo(UUID id) throws BusinessException;
 
-    int updateUserInfo(User user);
+    /**
+     * 获取用户的全部信息
+     * @param id 用户id
+     * @return UserFullInfoDTO
+     * @throws BusinessException 业务相关异常
+     */
+    UserFullInfoDTO getUserFullInfo(UUID id) throws BusinessException;
 
-    UserDTO login(String userID, String password);
+    /**
+     * 列出所有用户
+     * @return UserBasicInfoDTO[]
+     */
+    UserBasicInfoDTO[] listAllUser();
 
-    int register(User user);
+    /**
+     * 查询用户
+     * @return UserBasicInfoDTO[]
+     */
+    UserBasicInfoDTO[] queryUser();
+
+    /**
+     * 登录
+     * @param id 用户id
+     * @param password 登录密码
+     * @return 登录状态
+     * @throws BusinessException 业务相关异常
+     */
+    boolean login(UUID id, String password) throws BusinessException;
+
+    /**
+     * 注册用户
+     * @param userFullInfoDTO 用户信息DTO
+     * @return 生成的uuid
+     * @throws BusinessException 业务相关异常
+     */
+    UUID addUser(UserFullInfoDTO userFullInfoDTO) throws BusinessException;
+
+    /**
+     * 更新用户信息
+     * @param userFullInfoDTO 用户信息DTO
+     * @throws BusinessException 业务相关异常
+     */
+    void updateUser(UserFullInfoDTO userFullInfoDTO) throws BusinessException;
+
+    /**
+     * 删除用户
+     * @param id 用户id
+     * @throws BusinessException 业务相关异常
+     */
+    void deleteUser(UUID id) throws BusinessException;
+
+    /**
+     * 设置用户卖家权限
+     * @param id 用户id
+     * @param isSeller 是否有卖家权限
+     * @throws BusinessException 业务相关异常
+     */
+    void setUserSeller(UUID id,boolean isSeller) throws BusinessException;
+
+    /**
+     * 添加用户地址
+     * @param id 用户id
+     * @param addressDTO 地址DTO
+     * @throws BusinessException 业务相关异常
+     */
+    void addUserAddress(UUID id, AddressDTO addressDTO) throws BusinessException;
+
+    /**
+     * 删除用户地址
+     * @param userId 用户id
+     * @param addressId 地址id
+     * @throws BusinessException 业务相关异常
+     */
+    void deleteUserAddress(UUID userId,UUID addressId) throws BusinessException;
 }
--- src/main/java/org/acowzon/backend/service/user/impl/UserServiceImpl.java
+++ src/main/java/org/acowzon/backend/service/user/impl/UserServiceImpl.java
@@ -1,45 +1,142 @@
 package org.acowzon.backend.service.user.impl;
 
-import org.acowzon.backend.dto.user.UserDTO;
-import org.acowzon.backend.entity.user.User;
-import org.acowzon.backend.mapper.user.UserMapper;
+import org.acowzon.backend.dto.address.AddressDTO;
+import org.acowzon.backend.dto.user.UserBasicInfoDTO;
+import org.acowzon.backend.dto.user.UserFullInfoDTO;
+import org.acowzon.backend.exception.BusinessException;
 import org.acowzon.backend.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 
 @Service
 public class UserServiceImpl implements UserService {
-
-    @Autowired
-    UserMapper userMapper;
-
+    /**
+     * 获取用户的基本信息
+     *
+     * @param id 用户id
+     * @return UserBasicInfoDTO
+     * @throws BusinessException 业务相关异常
+     */
     @Override
-    public User getUserInfo(String userID) {
-        return this.userMapper.queryUserById(userID);
-    }
-
-
-    @Override
-    public int updateUserInfo(User user) {
-        return this.userMapper.updateUser(user);
-    }
-
-    @Override
-    public UserDTO login(String userID, String password) {
-        //System.out.println("service login "+customerBeans.get(0).toString());
-        //设置cookie,应答给客户
-        int res = this.userMapper.login(userID, password);
-        if (res > 0) {
-            User user = this.userMapper.queryUserById(userID);
-            //System.out.println(" sevice "+user.toString());
-            UserDTO userDTO = new UserDTO(userID, user.getUserNickname());
-            return userDTO;
-        }
+    public UserBasicInfoDTO getUserBasicInfo(UUID id) throws BusinessException {
         return null;
     }
 
-    public int register(User user) {
-        return this.userMapper.addUser(user);
+    /**
+     * 获取用户的全部信息
+     *
+     * @param id 用户id
+     * @return UserFullInfoDTO
+     * @throws BusinessException 业务相关异常
+     */
+    @Override
+    public UserFullInfoDTO getUserFullInfo(UUID id) throws BusinessException {
+        return null;
+    }
+
+    /**
+     * 列出所有用户
+     *
+     * @return UserBasicInfoDTO[]
+     */
+    @Override
+    public UserBasicInfoDTO[] listAllUser() {
+        return new UserBasicInfoDTO[0];
+    }
+
+    /**
+     * 查询用户
+     *
+     * @return UserBasicInfoDTO[]
+     */
+    @Override
+    public UserBasicInfoDTO[] queryUser() {
+        return new UserBasicInfoDTO[0];
+    }
+
+    /**
+     * 登录
+     *
+     * @param id       用户id
+     * @param password 登录密码
+     * @return 登录状态
+     * @throws BusinessException 业务相关异常
+     */
+    @Override
+    public boolean login(UUID id, String password) throws BusinessException {
+        return false;
+    }
+
+    /**
+     * 注册用户
+     *
+     * @param userFullInfoDTO 用户信息DTO
+     * @return 生成的uuid
+     * @throws BusinessException 业务相关异常
+     */
+    @Override
+    public UUID addUser(UserFullInfoDTO userFullInfoDTO) throws BusinessException {
+        return null;
+    }
+
+    /**
+     * 更新用户信息
+     *
+     * @param userFullInfoDTO 用户信息DTO
+     * @throws BusinessException 业务相关异常
+     */
+    @Override
+    public void updateUser(UserFullInfoDTO userFullInfoDTO) throws BusinessException {
+
+    }
+
+    /**
+     * 删除用户
+     *
+     * @param id 用户id
+     * @throws BusinessException 业务相关异常
+     */
+    @Override
+    public void deleteUser(UUID id) throws BusinessException {
+
+    }
+
+    /**
+     * 设置用户卖家权限
+     *
+     * @param id       用户id
+     * @param isSeller 是否有卖家权限
+     * @throws BusinessException 业务相关异常
+     */
+    @Override
+    public void setUserSeller(UUID id, boolean isSeller) throws BusinessException {
+
+    }
+
+    /**
+     * 添加用户地址
+     *
+     * @param id         用户id
+     * @param addressDTO 地址DTO
+     * @throws BusinessException 业务相关异常
+     */
+    @Override
+    public void addUserAddress(UUID id, AddressDTO addressDTO) throws BusinessException {
+
+    }
+
+    /**
+     * 删除用户地址
+     *
+     * @param userId    用户id
+     * @param addressId 地址id
+     * @throws BusinessException 业务相关异常
+     */
+    @Override
+    public void deleteUserAddress(UUID userId, UUID addressId) throws BusinessException {
+
     }
 }
