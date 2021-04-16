be92466526dd17b406a3f7d789e8e68072aadd21
cloudicen <cloudicen@foxmail.com>
2021-04-16 16:45:47 +0800

    暂时关闭权限拦截器功能

--- src/main/java/org/acowzon/backend/AcowzonApplication.java
+++ src/main/java/org/acowzon/backend/AcowzonApplication.java
@@ -2,8 +2,9 @@ package org.acowzon.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class AcowzonApplication {
     public static void main(String[] args) {
         SpringApplication.run(AcowzonApplication.class, args);
