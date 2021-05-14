78f2b981094df68bb47debcc7bd85a0ee18a7b85
cloudicen <cloudicen@foxmail.com>
2021-05-14 18:10:59 +0800

    移除spring-boot 官方的security依赖，使用自己实现的拦截器

--- src/main/java/org/acowzon/backend/AcowzonApplication.java
+++ src/main/java/org/acowzon/backend/AcowzonApplication.java
@@ -2,9 +2,8 @@ package org.acowzon.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-// @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+
 @SpringBootApplication
 public class AcowzonApplication {
     public static void main(String[] args) {
